cc66ec5db051cfd4a1cb88d6ae21f99db24db010
Sameer Kumar Satyadarshi <sameerkdarshi@gmail.com>
2019-03-27 21:29:46 +0530

    Added README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Writer
+### Simple text editor using JavaScript
+
+## To run this locally
+- Fork and Clone the repository
+- Run ```npm install```
+- Run ```live-server```
+- The application will launch itself in the browser.
--- app.js
+++ app.js
@@ -18,6 +18,29 @@ window.addEventListener("load", function () {
         editor.execCommand("InsertUnorderedList", false, "newOL" + Math.round(Math.random() *1000));
     }, false);
 
-    
+    fontColorButton.addEventListener("click", function (event) {
+        editor.execCommand("ForeColor", false, event.target.value);
+    }, false);
+
+    highlightButton.addEventListener("click", function (event) {
+        editor.execCommand("BackColor", false, event.target.value);
+    }, false);
+
+    fontChanger.addEventListener("click", function (event) {
+        console.log(event.target.value)
+        editor.execCommand("FontName", false, event.target.value);
+    }, false);
+
+    fontSizeChanger.addEventListener("click", function (event) {
+        editor.execCommand("FontSize", false, event.target.value);
+    }, false);
+
+    undoButton.addEventListener("click", function () {
+        editor.execCommand("Undo", false, null);
+    }, false);
+
+    redoButton.addEventListener("click", function () {
+        editor.execCommand("Redo", false, null);
+    }, false);
 
 }, false);
