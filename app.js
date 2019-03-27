846effee4d45692b114a696b183b9cc3a01cf067
Sameer Kumar Satyadarshi <sameerkdarshi@gmail.com>
2019-03-27 20:38:19 +0530

    Ordered List and Unordered List features added

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ window.addEventListener("load", function () {
         editor.execCommand("Italic", false, null);
     }, false);
 
+    orderedListButton.addEventListener("click", function () {
+        editor.execCommand("InsertOrderedList", false, "newOL" + Math.round(Math.random() *1000));
+    }, false);
+
+    unorderedListButton.addEventListener("click", function () {
+        editor.execCommand("InsertUnorderedList", false, "newOL" + Math.round(Math.random() *1000));
+    }, false);
+
+    
+
 }, false);
